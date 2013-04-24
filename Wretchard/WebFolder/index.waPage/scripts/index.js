867e0c1a281e31c69b93cd004ace8b35b4ece4a3
wretchard <wretchard@gmail.com>
2013-04-24 15:35:16 +1000

    Update #6

--- Wretchard/WebFolder/index.waPage/scripts/index.js
+++ Wretchard/WebFolder/index.waPage/scripts/index.js
@@ -2,24 +2,24 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItem1 = {};	// @menuItem
 	var menuItem3 = {};	// @menuItem
-	var menuItem2 = {};	// @menuItem
 	var menuItemTipjar = {};	// @menuItem
 	var menuItemPortfolio = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	menuItem1.click = function menuItem1_click (event)// @startlock
+	{// @endlock
+		$$('component_main').removeComponent();
+		$$('component_main').loadComponent('/Components/Ideas/Catering.waComponent');
+	};// @lock
+
 	menuItem3.click = function menuItem3_click (event)// @startlock
 	{// @endlock
 		$$('component_main').removeComponent();
 		$$('component_main').loadComponent('/Components/About.waComponent');
-	};// @lock
-
-	menuItem2.click = function menuItem2_click (event)// @startlock
-	{// @endlock
-		$$('component_main').removeComponent();
-		$$('component_main').loadComponent('/Components/UnderConstruction.waComponent');
 	};// @lock
 
 	menuItemTipjar.click = function menuItemTipjar_click (event)// @startlock
@@ -34,8 +34,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("menuItem1", "click", menuItem1.click, "WAF");
 	WAF.addListener("menuItem3", "click", menuItem3.click, "WAF");
-	WAF.addListener("menuItem2", "click", menuItem2.click, "WAF");
 	WAF.addListener("menuItemTipjar", "click", menuItemTipjar.click, "WAF");
 	WAF.addListener("menuItemPortfolio", "click", menuItemPortfolio.click, "WAF");
 // @endregion
--- Wretchard/httpDispatcher.js
+++ Wretchard/httpDispatcher.js
@@ -2,11 +2,9 @@
 	var responseTxt
 	if (settings.project.hostName=='localhost') {
 		 responseTxt='http://localhost:8081/index/'
-	     //response.headers.location ='http://local.tracker.com:8081/index/'
 	}
 	else {
 		responseTxt='http://' + settings.project.hostName + '/index/'
-		//response.headers.location ='http://' + settings.project.hostName + '/index/'
 	}
 	console.log(responseTxt);
 	response.headers.location=responseTxt;
