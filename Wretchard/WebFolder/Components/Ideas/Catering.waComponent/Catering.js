82d0e57510b99fceb2a65908350214f4646b5910
wretchard <wretchard@gmail.com>
2013-04-24 20:38:38 +1000

    Update #7

--- Wretchard/WebFolder/Components/Ideas/Catering.waComponent/Catering.js
+++ Wretchard/WebFolder/Components/Ideas/Catering.waComponent/Catering.js
@@ -11,17 +11,37 @@ function constructor (id) {
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
-		debugger;
-		sources.component_main_ideasCatering.all();
-		component_main_arrayFood=sources.component_main_ideasCatering.toArray();
-
+		var v = sources.component_main_ideasCatering.all({
+			onSuccess: function(event) {
+				buildArray() 
+				}
+			}
+		);
 	// @region namespaceDeclaration// @startlock
+	var containerMenu = {};	// @container
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	containerMenu.click = function containerMenu_click (event)// @startlock
+	{// @endlock
+		alert('detail form');
+	};// @lock
+	
+	function buildArray() {
+	component_main_arrayFood=[];
+	for (var i=0; i< sources.component_main_ideasCatering.length; i++) {
+	   if(sources.component_main_ideasCatering.getPosition()<=i) {
+	 	  component_main_arrayFood.push({url:sources.component_main_ideasCatering.url, blurb:sources.component_main_ideasCatering.blurb})  	
+	   	}
+	   	sources.component_main_ideasCatering.selectNext();
+	}
+		sources.component_main_arrayFood.sync();
+	};
 	
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_containerMenu", "click", containerMenu.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
