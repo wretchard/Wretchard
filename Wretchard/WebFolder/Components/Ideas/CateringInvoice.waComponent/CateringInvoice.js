e72985c82c1d6f371c19fe3d4ab0746d066c7ca7
wretchard <wretchard@gmail.com>
2013-04-28 12:14:55 +1000

    Changes To Homepage

--- Wretchard/WebFolder/Components/Ideas/CateringInvoice.waComponent/CateringInvoice.js
+++ Wretchard/WebFolder/Components/Ideas/CateringInvoice.waComponent/CateringInvoice.js
@@ -33,11 +33,24 @@ function constructor (id) {
 		
 
 	// @region namespaceDeclaration// @startlock
+	var radioGroup1 = {};	// @radioGroup
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	radioGroup1.change = function radioGroup1_change (event)// @startlock
+	{// @endlock
+		//alert(this._value);
+		debugger;
+		sources.component_main_componentList_ideasInvoice.addNewElement();
+		var Clist=sources.component_main_componentList_ideasInvoice.getCurrentElement()
+		Clist.quantity=5;
+		Clist.save();
+		Clist.resolveSource();
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_radioGroup1", "change", radioGroup1.change, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
--- Wretchard/WebFolder/index.waPage/scripts/index.js
+++ Wretchard/WebFolder/index.waPage/scripts/index.js
@@ -2,14 +2,26 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItemNoWay = {};	// @menuItem
+	var menuItemBelmont = {};	// @menuItem
 	var menuItemRecruitment = {};	// @menuItem
 	var menuItem1 = {};	// @menuItem
 	var menuItem3 = {};	// @menuItem
 	var menuItemTipjar = {};	// @menuItem
-	var menuItemPortfolio = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	menuItemNoWay.click = function menuItemNoWay_click (event)// @startlock
+	{// @endlock
+		window.open("http://www.amazon.com/exec/obidos/ASIN/1453892818/wwwfallbackbe-20", "_blank")
+	};// @lock
+
+	menuItemBelmont.click = function menuItemBelmont_click (event)// @startlock
+	{// @endlock
+		$$('component_main').removeComponent();
+		$$('component_main').loadComponent('/Components/Portfolio/Belmont.waComponent');
+	};// @lock
 
 	menuItemRecruitment.click = function menuItemRecruitment_click (event)// @startlock
 	{// @endlock
@@ -36,17 +48,12 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		//window.location.href='/tipjar.html';
 	};// @lock
 
-	menuItemPortfolio.click = function menuItemPortfolio_click (event)// @startlock
-	{// @endlock
-		$$('component_main').removeComponent();
-		$$('component_main').loadComponent('/Components/UnderConstruction.waComponent');
-	};// @lock
-
 // @region eventManager// @startlock
+	WAF.addListener("menuItemNoWay", "click", menuItemNoWay.click, "WAF");
+	WAF.addListener("menuItemBelmont", "click", menuItemBelmont.click, "WAF");
 	WAF.addListener("menuItemRecruitment", "click", menuItemRecruitment.click, "WAF");
 	WAF.addListener("menuItem1", "click", menuItem1.click, "WAF");
 	WAF.addListener("menuItem3", "click", menuItem3.click, "WAF");
 	WAF.addListener("menuItemTipjar", "click", menuItemTipjar.click, "WAF");
-	WAF.addListener("menuItemPortfolio", "click", menuItemPortfolio.click, "WAF");
 // @endregion
 };// @endlock
